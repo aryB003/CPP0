e9f9fcce1811f54fd1da565d72ffe236205e26fc
Aryan <abardhan@student.42abudhabi.ae>
2026-09-10 22:25:21 +0400

    test code

--- megaphone.cpp
+++ megaphone.cpp
@@ -6,13 +6,29 @@
 /*   By: aryan <aryan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/10 21:09:55 by aryan             #+#    #+#             */
-/*   Updated: 2026/09/10 21:57:53 by aryan            ###   ########.fr       */
+/*   Updated: 2026/09/10 22:24:29 by aryan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(int argc, char **av)
+int main(int ac, char **av)
 {
-
+    if(ac < 2)
+        std::cout<<"* LOUD AND UNBEARABLE BUZZER NOISE*";
+    else
+    {
+        for(int i = 1; i < ac; i++)
+        {
+            std::string result = std::string(av[i]);
+            for (int j = 0; j < (int)result.size(); j++)
+            {
+                std::cout << (char)std::toupper(result[j]);
+            }
+            if (av[i + 1])
+                std::cout << " ";   
+        }
+        std::cout << std::endl;
+    }
+    return(0);
 }
